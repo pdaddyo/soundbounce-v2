39676882c96cd77419bf5b4f92ccd25e2fe758dd
Paul Barrass <paul.barrass@gmail.com>
2017-04-20 14:45:55 +0100

    Add production mode to compile script

--- server/main.js
+++ server/main.js
@@ -23,7 +23,7 @@ soundbounce.init();
 if (config.env === 'development') {
 	const compiler = webpack(webpackConfig);
 
-	debug('Enabling webpack dev and HMR middleware');
+	debug('DEV MODE - Enabling webpack dev and HMR middleware');
 	app.use(require('webpack-dev-middleware')(compiler, {
 		publicPath: webpackConfig.output.publicPath,
 		contentBase: paths.base(config.dir_client),
@@ -43,32 +43,33 @@ if (config.env === 'development') {
 		path: '/__webpack_hmr'
 	}));
 
-	// Serve static assets from ~/public since Webpack is unaware of
-	// these files. This middleware doesn't need to be enabled outside
-	// of development since this directory will be copied into ~/dist
-	// when the application is compiled.
-	app.use(express.static(paths.base(config.dir_dist)));
-
-	// This rewrites all routes requests to the root /index.html file
-	// (ignoring file requests). If you want to implement universal
-	// rendering, you'll want to remove this middleware.
-	app.use('*', function (req, res, next) {
-		console.log('*');
-		const filename = path.join(compiler.outputPath, 'index.html');
-		compiler.outputFileSystem.readFile(filename, (err, result) => {
-			if (err) {
-				return next(err);
-			}
-			res.set('content-type', 'text/html');
-			res.send(result);
-			res.end();
-		});
-	});
 } else {
-	// Serving ~/dist by default. Ideally these files should be served by
-	// the web server and not the app server, but this helps to demo the
-	// server in production.
-	app.use(express.static(paths.base(config.dir_dist)));
+	// production server mode
+	debug('PRODUCTION MODE - hot reloading disabled');
 }
+
+// shared between dev + prod servers
+
+// Serve static assets from ~/public since Webpack is unaware of
+// these files. This middleware doesn't need to be enabled outside
+// of development since this directory will be copied into ~/dist
+// when the application is compiled.
+app.use(express.static(paths.base(config.dir_dist)));
+
+// This rewrites all routes requests to the root /index.html file
+// (ignoring file requests). If you want to implement universal
+// rendering, you'll want to remove this middleware.
+app.use('*', function (req, res, next) {
+	console.log('*');
+	const filename = path.join(compiler.outputPath, 'index.html');
+	compiler.outputFileSystem.readFile(filename, (err, result) => {
+		if (err) {
+			return next(err);
+		}
+		res.set('content-type', 'text/html');
+		res.send(result);
+		res.end();
+	});
+});
 
 export default app;
