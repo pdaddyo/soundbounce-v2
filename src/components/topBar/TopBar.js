16f11cb8fc16c13985ba02c2debe584ecea3f23e
Paul Barrass <paul.barrass@gmail.com>
2017-04-23 00:06:20 +0100

    Tweak top bar styles

--- src/components/topBar/TopBar.js
+++ src/components/topBar/TopBar.js
@@ -1,20 +1,46 @@
-import React, {Component} from 'react';
+import React, {Component, PropTypes} from 'react';
 import {connect} from 'react-redux';
-import SpotifyPlayerStatus from 'components/player/SpotifyPlayerStatus';
+// import SpotifyPlayerStatus from 'components/player/SpotifyPlayerStatus';
 
 import classes from './topBar.css';
 
 class TopBar extends Component {
-	static propTypes = {};
+	static propTypes = {
+		profile: PropTypes.object,
+		player: PropTypes.object
+	};
 
 	render() {
+		const {profile, player} = this.props;
+
+		let avatarImg = 'none';
+		if (profile && profile.images && profile.images.length > 0) {
+			avatarImg = `url(${profile.images[0].url})`;
+		}
+		// const [, firstName] = profile.display_name.match(/(.+) /);
 		return (
 			<div className={classes.topBar}>
 				<div className={classes.soundbounce}>
 					Soundbounce
 				</div>
-				<div className={classes.playerStatus}>
-					<SpotifyPlayerStatus/>
+				<div className={classes.right}>
+					{/* <SpotifyPlayerStatus/> */}
+
+					<div className={classes.user}>
+
+						<div className={classes.text}>
+							<div className={classes.profileName}>
+								Bouncing <span className={classes.room}>Boomtown</span>
+							</div>
+							<div className={classes.device}>
+								to {player && player.device.name}
+								<span className={classes.dropdownArrow}> ▼ </span>
+							</div>
+						</div>
+						<div className={classes.avatar}
+							 style={{backgroundImage: avatarImg}}/>
+					</div>
+
 				</div>
 			</div>
 		);
@@ -24,7 +50,7 @@ class TopBar extends Component {
 // map the spotify player state to prop 'player'
 const mapStateToProps = state => ({
 	profile: state.spotify.profile,
-	devices: state.spotify.devices
+	player: state.spotify.player
 });
 
 const mapDispatchToProps = (dispatch, ownProps) => ({});
