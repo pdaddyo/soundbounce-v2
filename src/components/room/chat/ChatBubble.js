f030c95cb5ab159943a773d44802fbeda9cae4b3
Paul Barrass <paul.barrass@gmail.com>
2017-05-11 21:59:57 +0100

    Make solo emojis display larger #10

--- src/components/room/chat/ChatBubble.js
+++ src/components/room/chat/ChatBubble.js
@@ -9,10 +9,25 @@ import Avatar from '../../user/avatar/Avatar';
 import Linkify from 'react-linkify';
 import ReactEmoji from 'react-emoji';
 
+const soloEmojiSize = '30px';
+
 export default class ChatBubble extends Component {
 	static propTypes = {
 		chat: PropTypes.object.isRequired
 	};
+
+	emojify(text) {
+		const emojifiedText = ReactEmoji.emojify(text);
+		if (Array.isArray(emojifiedText) &&
+			emojifiedText.length === 1 &&
+			typeof emojifiedText[0] !== 'string') {
+			// we have a single emoji here, so render it bigger!
+			return ReactEmoji.emojify(text,
+				{attributes: {width: soloEmojiSize, height: soloEmojiSize}}
+			);
+		}
+		return emojifiedText;
+	}
 
 	render() {
 		const {chat} = this.props;
@@ -32,7 +47,7 @@ export default class ChatBubble extends Component {
 					{chat.payloads.map((chat, index) => (
 						<div className={theme.text} key={index}>
 							<Linkify properties={{target: '_blank'}}>
-								{ReactEmoji.emojify(chat.text.trim())}
+								{this.emojify(chat.text.trim())}
 							</Linkify>
 						</div>
 					))}
