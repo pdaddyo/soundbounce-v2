0644fe5590bfa1071fd5de196a93e92f3604d498
Paul Barrass <paul.barrass@gmail.com>
2017-10-16 23:57:23 +0100

    Bump version

--- config/app.js
+++ config/app.js
@@ -6,7 +6,7 @@
 
 export default {
 	// incrementing this forces clients to download new client code in order to connect
-	buildVersion: 9,
+	buildVersion: 10,
 	spotify: {
 		webApiBaseUrl: 'https://api.spotify.com',
 		pollPlayerDelay: 5000,
